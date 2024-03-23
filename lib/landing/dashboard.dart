4443ec9b1c36c099e92e35576bebaa35f97e7730
debadrita438 <bristibandyopadhyay@gmail.com>
2024-03-23 12:59:48 +0530

    font and search field added

--- lib/landing/dashboard.dart
+++ lib/landing/dashboard.dart
@@ -1,9 +1,24 @@
+import 'package:coffee_shop/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 import 'package:coffee_shop/common_widgets/index.dart';
+import 'package:hexcolor/hexcolor.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  final _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +26,36 @@ class DashboardScreen extends StatelessWidget {
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 45),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const MainHeader(),
+            const SizedBox(height: 25),
+            const Text(
+              'Find the best\ncoffee for you',
+              style: TextStyle(
+                fontSize: 28,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 25),
+            TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: 'Find your coffee...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+                fillColor: const Color.fromARGB(255, 28, 31, 38),
+                filled: true,
+                hintStyle: TextStyle(
+                  color: HexColor(AppColor.lightGray),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 10,
+                ),
+              ),
+            )
           ],
         ),
       ),
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,4 +2,6 @@ class AppColor {
   static const deepestBlue = '#0C0F14';
   static const grayishBlack = '#21262E';
   static const transparentWhite = '#2EFFFFFF';
+  static const lightGray = '#52555A';
+  static const darkBlue = '#141921';
 }
